f20df506210abdce6bec8a990572a763befda55a
chrisarrow90 <chrisarrowsmith@gmail.com>
2021-11-01 23:31:54 +1300

    restructure - move infrastructure folder to src directory. remove navigation code from app.js into separate file

---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Text } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "../../components/utility/safe-area.component";
+
+function SettingsScreen() {
+  return (
+    <SafeArea>
+      <Text>Settings!</Text>
+    </SafeArea>
+  );
+}
+
+function MapScreen() {
+  return (
+    <SafeArea>
+      <Text>Map!</Text>
+    </SafeArea>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Settings: "md-settings",
+  Map: "md-map",
+};
+
+const createScreenOptions = ({ route }) => {
+  let iconName = TAB_ICON[route.name];
+
+  return {
+    tabBarIcon: ({ focused, size, color }) => (
+      <Ionicons name={focused ? `${iconName}-outline` : iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+    headerShown: false,
+  };
+};
+
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={createScreenOptions}>
+        <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
