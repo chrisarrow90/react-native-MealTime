a407f2985f457aafb5817c6d30ff41ef661ac1a6
chrisarrow90 <chrisarrowsmith@gmail.com>
2021-11-02 22:34:41 +1300

    add restaurant image and title to map callout

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { MapCallout } from "../components/map-callout.component";
 
 import { Search } from "../components/search.component";
 
@@ -46,7 +47,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            ></MapView.Marker>
+            >
+              <MapView.Callout>
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
           );
         })}
       </Map>
