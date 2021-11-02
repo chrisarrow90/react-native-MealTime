fda874f5b966a3048e93e92f20c46f37a187a363
chrisarrow90 <chrisarrowsmith@gmail.com>
2021-11-02 22:38:19 +1300

    fix import error for WebView. add onPress function on map callout to navigate to restaruant detail

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
-import WebView from "react-native-webview";
+import { WebView } from "react-native-webview";
 import { Platform } from "react-native";
 
 import { Text } from "../typography/text.component";
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -13,7 +13,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
   const [latDelta, setLatDelta] = useState(0);
@@ -48,7 +48,9 @@ export const MapScreen = () => {
                 longitude: restaurant.geometry.location.lng,
               }}
             >
-              <MapView.Callout>
+              <MapView.Callout
+                onPress={() => navigation.navigate("RestaurantDetail", { restaurant })}
+              >
                 <MapCallout restaurant={restaurant} />
               </MapView.Callout>
             </MapView.Marker>
