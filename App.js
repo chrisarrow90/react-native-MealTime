b8fa99b4c57bb18ba079b106dff1e1b891e0553e
chrisarrow90 <chrisarrowsmith@gmail.com>
2021-11-03 14:20:24 +1300

    install firebase and configure. setup authentication context

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
 
-import {
-  useFonts as useOswald,
-  Oswald_400Regular,
-} from "@expo-google-fonts/oswald";
+import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme/index";
+import { Navigation } from "./src/infrastructure/navigation";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
-import { Navigation } from "./src/infrastructure/navigation";
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCahvjrTKj6lPH1Bqbm_bhSYz3rkG9zKFo",
+  authDomain: "mealtime-cc710.firebaseapp.com",
+  projectId: "mealtime-cc710",
+  storageBucket: "mealtime-cc710.appspot.com",
+  messagingSenderId: "779540026848",
+  appId: "1:779540026848:web:f0bf7930bf9991bb3b0785",
+};
+
+// prevent hot reload error due to firebase sdk in expo
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -32,13 +45,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
