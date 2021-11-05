6dd862f95b8858f2351e14a87baa971249156988
chrisarrow90 <chrisarrowsmith@gmail.com>
2021-11-05 16:59:46 +1300

    complete login screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Button } from "react-native-paper";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import {
   AccountBackground,
   AccountContainer,
   AccountCover,
   AuthButton,
+  Title,
 } from "../components/account.styles";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>MealTime</Title>
       <AccountContainer>
         <AuthButton
           icon="lock-open-outline"
@@ -21,11 +22,7 @@ export const AccountScreen = ({ navigation }) => {
           Login
         </AuthButton>
         <Spacer position="top" size="large" />
-        <AuthButton
-          icon="lock-open-outline"
-          mode="contained"
-          onPress={() => navigation.navigate("Register")}
-        >
+        <AuthButton icon="email" mode="contained" onPress={() => navigation.navigate("Register")}>
           Register
         </AuthButton>
       </AccountContainer>
--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,10 +1,68 @@
-import React from "react";
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import React, { useContext, useState } from "react";
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+  AuthInput,
+  ErrorContainer,
+  Title,
+} from "../components/account.styles";
 
-export const LoginScreen = () => {
+import { Text } from "../../../components/typography/text.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { Navigation } from "../../../infrastructure/navigation";
+
+export const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { onLogin, error } = useContext(AuthenticationContext);
+
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>MealTime</Title>
+      <AccountContainer>
+        <AuthInput
+          label="Email"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(e) => setEmail(e)}
+        />
+        <Spacer size="large">
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            secure
+            onChangeText={(p) => setPassword(p)}
+          />
+        </Spacer>
+        {error && (
+          <ErrorContainer>
+            <Text variant="error">{error}</Text>
+          </ErrorContainer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => onLogin(email, password)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
     </AccountBackground>
   );
 };
